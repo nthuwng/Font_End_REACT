840e6b06aa578e3f3b41240a7875e793172f38c6
Git cua Hung <nguyenthinhhung150603@gmail.com>
2025-02-15 17:33:48 +0700

    video 30

--- src/components/learn/Mycomponent.jsx
+++ src/components/learn/Mycomponent.jsx
@@ -1,5 +1,14 @@
+import "./style.css";
+
 const MyFunction = () => {
-  return <div>HungDev learn React</div>;
+  return (
+    <>
+      <div>HungDev learn React</div>
+      <div className="child" style={{ borderRadius: "10px" }}>
+        child
+      </div>
+    </>
+  );
 };
 
 export default MyFunction; // dùng để export 1 function ra ngoài
