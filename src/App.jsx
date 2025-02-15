95c7125fd7914354cd8a45afa604ce0a11267ea1
Git cua Hung <nguyenthinhhung150603@gmail.com>
2025-02-15 19:13:14 +0700

    video 34

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,17 @@ import "./components/todo/todo.css";
 import reactLogo from "./assets/react.svg";
 
 const App = () => {
+  const hoidanit = "Eric Arsenal";
+  const age = 25;
+  const data = {
+    address: "hanoi",
+    country: "vietnam",
+  };
   return (
     <div className="todo-container">
       <div className="todo-title">Todo List</div>
       <TodoNew />
-      <TodoData />
+      <TodoData name={hoidanit} age={age} data={data} />
       <div className="todo-image">
         <img src={reactLogo} className="logo" />
       </div>
--- src/components/todo/TodoData.jsx
+++ src/components/todo/TodoData.jsx
@@ -1,10 +1,24 @@
-const TodoData = () => {
-    return (
-        <div className='todo-data'>
-            <div> Learning React </div>
-            <div> Watching Youtube</div>
-        </div>
-    )
-}
+const TodoData = (props) => {
+  //props là một object {}
+  // {
+  //     name: "ERic",
+  //     age: 25,
+  //     data: {}
+  // }
+  const { name, age, data } = props;
+  // const name = props.name;
+  // const age = props.age;
+  // const data = props.data;
+
+  // console.log(">>> check props: ", props)
+
+  return (
+    <div className="todo-data">
+      <div>My name is {name}</div>
+      <div> Learning React </div>
+      <div> Watching Youtube</div>
+    </div>
+  );
+};
 
 export default TodoData;
