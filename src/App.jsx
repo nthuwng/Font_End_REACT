30352c252ca5daf95a324207bc7420f893781a37
Git cua Hung <nguyenthinhhung150603@gmail.com>
2025-02-15 16:25:50 +0700

    video 27

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,13 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
-function App() {
+const App = () => {
   const [count, setCount] = useState(0);
 
+  const myFunction = () => {
+    console.log("Hello from myFunction");
+  };
+  myFunction();
   return (
     <>
       <div>
@@ -30,6 +34,6 @@ function App() {
       </p>
     </>
   );
-}
+};
 
 export default App;
