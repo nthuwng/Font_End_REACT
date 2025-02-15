c5034369538a7a1c4b951cf67175d38456085c1b
Git cua Hung <nguyenthinhhung150603@gmail.com>
2025-02-15 17:42:42 +0700

    video 31

--- src/components/learn/Mycomponent.jsx
+++ src/components/learn/Mycomponent.jsx
@@ -1,9 +1,21 @@
 import "./style.css";
 
 const MyFunction = () => {
+  // const hoidanit = "Hoi Dan IT"; // string
+  // const hoidanit = 123; // number
+  // const hoidanit = undefined;
+  // const hoidanit = null;
+  // const hoidanit = true;  // boolean
+  // const hoidanit = [1, 2, 3]; //array
+  const hoidanit = {
+    name: "Hoi Dan IT",
+    age: 22,
+  };
+
   return (
     <>
-      <div>HungDev learn React</div>
+      <div>{JSON.stringify(hoidanit)} & learn React</div>
+      {console.log("Edric")}
       <div className="child" style={{ borderRadius: "10px" }}>
         child
       </div>
