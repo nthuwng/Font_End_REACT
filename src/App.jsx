681429c1d40c7f5cd254f09b61a916585df65f84
Git cua Hung <nguyenthinhhung150603@gmail.com>
2025-02-16 18:12:21 +0700

    video 42

--- src/App.jsx
+++ src/App.jsx
@@ -23,6 +23,10 @@ const App = () => {
     setTodoList([...todoList, newTodo]);
   };
 
+  const deleteTodo = (id) => {
+    const newTodo = todoList.filter((item) => item.id !== id);
+    setTodoList(newTodo);
+  };
   function randomIntFromInterval(min, max) {
     // min and max included
     return Math.floor(Math.random() * (max - min + 1) + min);
@@ -32,17 +36,13 @@ const App = () => {
     <div className="todo-container">
       <div className="todo-title">Todo List</div>
       <TodoNew addNewTodo={addNewTodo} />
-      {todoList.length > 0 ? 
-        (
-          <TodoData todoList={todoList} />
-        )
-        :
-        (
-          <div className="todo-image">
-            <img src={reactLogo} className="logo" />
-          </div>
-        )
-      }
+      {todoList.length > 0 ? (
+        <TodoData todoList={todoList} deleteTodo={deleteTodo} />
+      ) : (
+        <div className="todo-image">
+          <img src={reactLogo} className="logo" />
+        </div>
+      )}
     </div>
   );
 };
--- src/components/todo/TodoData.jsx
+++ src/components/todo/TodoData.jsx
@@ -1,14 +1,20 @@
 const TodoData = (props) => {
-  const { todoList } = props;
+  const { todoList, deleteTodo } = props;
 
+  const handleClick = (id) => {
+    deleteTodo(id);
+  };
   return (
     <div className="todo-data">
       {todoList.map((item, index) => {
-        console.log(">>> check item: ", item, index);
         return (
           <div className={`todo-item`} key={item.id}>
             <div>{item.name}</div>
-            <button>Delete</button>
+            <button
+              style={{ cursor: "pointer" }}
+              onClick={() => handleClick(item.id)}>
+              Delete
+            </button>
           </div>
         );
       })}
