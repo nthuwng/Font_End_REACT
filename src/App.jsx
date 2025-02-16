6ad199f841d20564e8da4079a46e5af7a12f1506
Git cua Hung <nguyenthinhhung150603@gmail.com>
2025-02-16 17:51:13 +0700

    video 41

--- src/App.jsx
+++ src/App.jsx
@@ -32,10 +32,17 @@ const App = () => {
     <div className="todo-container">
       <div className="todo-title">Todo List</div>
       <TodoNew addNewTodo={addNewTodo} />
-      <TodoData todoList={todoList} />
-      <div className="todo-image">
-        <img src={reactLogo} className="logo" />
-      </div>
+      {todoList.length > 0 ? 
+        (
+          <TodoData todoList={todoList} />
+        )
+        :
+        (
+          <div className="todo-image">
+            <img src={reactLogo} className="logo" />
+          </div>
+        )
+      }
     </div>
   );
 };
