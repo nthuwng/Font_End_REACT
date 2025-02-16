702c1126af415d58eeb67a2d5ad5d2ee68bed35f
Git cua Hung <nguyenthinhhung150603@gmail.com>
2025-02-16 14:49:36 +0700

    video 40

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,15 @@ import { useState } from "react";
 
 const App = () => {
   const [todoList, setTodoList] = useState([
-    {
-      id: 1,
-      name: "Learning React",
-    },
-    {
-      id: 2,
-      name: "Watching Youtube",
-    },
+    // {
+    //   id: 1,
+    //   name: "Learning React",
+    // },
+    // {
+    //   id: 2,
+    //   name: "Watching Youtube",
+    // },
   ]);
-  const hoidanit = "Eric Arsenal";
-  const age = 25;
-  const data = {
-    address: "hanoi",
-    country: "vietnam",
-  };
   const addNewTodo = (name) => {
     const newTodo = {
       id: randomIntFromInterval(1, 1000000),
@@ -38,7 +32,7 @@ const App = () => {
     <div className="todo-container">
       <div className="todo-title">Todo List</div>
       <TodoNew addNewTodo={addNewTodo} />
-      <TodoData name={hoidanit} age={age} data={data} todoList={todoList} />
+      <TodoData todoList={todoList} />
       <div className="todo-image">
         <img src={reactLogo} className="logo" />
       </div>
